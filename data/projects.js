4056060e80af610403634e87e4735f20150711f7
Mukul <74971960+mk-mukul@users.noreply.github.com>
2023-09-16 04:04:56 +0530

    add algo id in ui

--- data/projects.js
+++ data/projects.js
@@ -2,7 +2,7 @@
 const projects = [
   {
     title: "Algorithmic Trading Application",
-    projectLink: "/algo_trading?algo_id=testingLogs",
+    projectLink: "/algo_trading?algo_id=testing",
     img: {
       link: "/img/algotrading-result.png",
       alt: "algotrading-result",
@@ -18,7 +18,7 @@ const projects = [
     links: [
       {
         for: "website",
-        link: "/algo_trading?algo_id=testingLogs"
+        link: "/algo_trading?algo_id=testing"
       },
     ],
   },
--- pages/algo_trading.js
+++ pages/algo_trading.js
@@ -88,13 +88,16 @@ export default function AlgoTradnig() {
           ></div>
         </div>
 
+        <div className=" w-full rounded-t font-semibold bg-gray-900 flex justify-center">
+        <div className=" px-2 self-center text-gray-50 pt-1 text-lg md:text-2xl">{`Algo ID = ${algo_id}`}</div>
+        </div>
         {algoLogs.length?<div className=" w-full block relative">
-          <input className={`${search?" bg-gray-600":" bg-gray-700"} z-50 text-white px-2 py-1 absolute right-2 top-2 rounded border-0 focus:outline-none`}
+          <input className={`${search?" bg-gray-600":" bg-gray-700"} z-10 text-white px-2 py-1 absolute right-2 top-2 rounded border-0 focus:outline-none`}
           placeholder="search logs"
           onChange={(e)=>{setSearch(e.target.value)}}
           />
         </div>:<></>}
-        <div className=" z-0 rounded bg-gray-900 w-full h-fit max-h-full px-1 md:px-2 lg:px-4 py-2 overflow-auto">
+        <div className=" z-0 rounded-b bg-gray-900 w-full h-fit max-h-full px-1 md:px-2 lg:px-4 py-2 overflow-auto">
 
         {algoLogs.map((log,ind)=>{
           const time_stamp = log.asctime.split(' ')[1].split(',')[0]
