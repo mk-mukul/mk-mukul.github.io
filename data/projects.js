0bb651a47a4cd5ef177f52462066ef21dbc59fbf
Mukul <mk-mukul@users.noreply.github.com>
2026-05-14 02:09:13 +0530

    update trading link

--- data/projects.js
+++ data/projects.js
@@ -24,7 +24,7 @@ const projects = [
   },
   {
     title: "Algorithmic Trading Application",
-    projectLink: "https://trading-dev.mkmukul.com/algo_trading?algo_id=testing",
+    projectLink: "https://trading.mkmukul.com/algo/logs/",
     img: {
       link: "/img/algotrading-result.png",
       alt: "algotrading-result",
@@ -36,7 +36,7 @@ const projects = [
     links: [
       {
         for: "website",
-        link: "https://trading-dev.mkmukul.com/algo_trading?algo_id=testing"
+        link: "https://trading.mkmukul.com/algo/logs/"
       },
     ],
   },
